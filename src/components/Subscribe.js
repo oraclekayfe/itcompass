358170866ca24f88c96afe54015c84ed69a4a37c
orc <orc@orc.orc>
2020-11-15 00:12:19 +0100

    working on about

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,32 +1,27 @@
 import React from "react"
 
 const Subscribe = () => {
-    return (
-        <section className="section-center">
-            <div className="newsletter-form">
-                <article className="contact-form">
-                    <h3>Get Notified of New Tutorials & Blog Post</h3>
-                    <form
-                        action="https://formspree.io/f/xjvprnqq"
-                        method="POST"
-                    >
-                        <div className="form-group">
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="Enter Email..."
-                                className="form-control"
-                            />
-                        </div>
-                        <button type="submit" className="btn submit-btn">
-                            Subscribe
-                        </button>
-                    </form>
-                </article>
-                <p>Your Email Address will Never Be Shared</p>
-            </div>
-        </section>
-    )
+  return (
+    <div className="newsletter-form">
+      <article className="contact-form">
+        <h3>Get Notified of New Tutorials & Blog Posts</h3>
+        <form action="https://formspree.io/f/xjvprnqq" method="POST">
+          <div>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter Email..."
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="btn submit-btn">
+            Subscribe
+          </button>
+        </form>
+      </article>
+      <p>Your Email Address will Never Be Shared</p>
+    </div>
+  )
 }
 
 export default Subscribe
